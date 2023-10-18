f1398f2c64b389f478b1931ec4a7e7788d8a92f5
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-10-18 07:28:25 +0000

    [add]rootの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,14 @@ devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
-  devise_for :users
+  
+  root to: 'public/homes#top'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
