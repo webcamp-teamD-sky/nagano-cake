624fa4c060e41dd3450dfb7729af72d8e8a5d969
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-10-19 07:14:35 +0000

    [add] association

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :cart_items, dependent: :destroy
+  has_many :items, through: :cart_items
+  # 上記、エラー吐く可能性あり。 
+  # エラー吐いた場合上記をコメントアウトすると解決する可能性あり。
+  has_many :orders, dependent: :destroy
+  has_many :addresses, dependent: :destroy
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :customers, through: :cart_items
+  has_many :order_details, dependent: :destroy
+  belongs_to :genre
 end
