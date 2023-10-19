a8d567661471db840b703b0dc09727a5b4b4fc91
myf2ar10 <myf2ar10@gmail.com>
2023-10-19 05:32:31 +0000

    [fix]orders/new routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,4 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/check'
-    get 'orders/completion'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/check'
-    get 'customers/withdraw'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
@@ -44,8 +11,16 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
-  
+
   root to: 'public/homes#top'
-  
+
+namespace :public, path: '' do
+  resources :orders, only: [:new, :index, :show, :create] do
+    collection do
+      get :check
+      get :completion
+    end
+  end
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
