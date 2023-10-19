eff6b30df9b87193f3ee90b6e2c245c885064d2e
myf2ar10 <myf2ar10@gmail.com>
2023-10-19 00:18:56 +0000

    [add]public_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,37 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+    get 'addresses/create'
+    get 'addresses/update'
+    get 'addresses/destroy'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/check'
+    get 'orders/completion'
+    get 'orders/create'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/destroy'
+    get 'cart_items/destroy_all'
+    get 'cart_items/create'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/check'
+    get 'customers/withdraw'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
