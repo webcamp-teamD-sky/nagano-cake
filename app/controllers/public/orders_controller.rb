59eb88926248abbbf919b9ac814d78ab0e616522
myf2ar10 <myf2ar10@gmail.com>
2023-10-20 21:30:59 +0000

    [fix]controller_spel

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,17 +12,17 @@ class Public::OrdersController < ApplicationController
     @cart_items.each do |cart_item|
       ary << (cart_item.item.price*1.1 *cart_item.amount).floor
     end
-      if params[:order][:address_oprion] == "0"
+      if params[:order][:address_option] == "0"
       @order.postcode = current_customer.postcode
       @order.address = current_customer.address
       @order.address_name = current_customer.first_name + current_customer.last_name
     elsif params[:order][:address_option] == "1"
-      @address = Address.fnd(params[:order][:order_address]
+      @address = Address.fnd(params[:order][:order_address])
       @order.postcode = @address.postcode
       @order.address = @address.address
       @order.address_name = @address.name
 
-    elsif params[:order][:address_option] = "2"
+    elsif params[:order][:address_option] == "2"
       @order.postcode = params[:order][:postcode]
       @order.address = params[:order][:address]
       @order.address_name = params[:order][:name]
@@ -50,17 +50,16 @@ class Public::OrdersController < ApplicationController
     @order = current_customer.orders.new(order_params)
     @order.order_status = 0
       if @order.save
-        current_customer.cart_items.each do |cart_item| #カート内商品を1つずつ取り出しループ
-          @ordered_item = OrderDetail.new #初期化宣言
-          @ordered_item.order_id =  @order.id #order注文idを紐付けておく
-          @ordered_item.item_id = cart_item.item_id #カート内商品idを注文商品idに代入
-          @ordered_item.quantity = cart_item.amount #カート内商品の個数を注文商品の個数に代入
-          @ordered_item.price = (cart_item.item.price*1.1).floor #消費税込みに計算して代入
+        current_customer.cart_items.each do |cart_item|
+          @ordered_item = OrderDetail.new
+          @ordered_item.order_id =  @order.id
+          @ordered_item.item_id = cart_item.item_id
+          @ordered_item.quantity = cart_item.amount
+          @ordered_item.price = (cart_item.item.price*1.1).floor
           @ordered_item.save
         end
           redirect_to public_orders_complete_path
             cart_items.destroy_all
-    # ユーザーに関連するカートのデータ(購入したデータ)をすべて削除します(カートを空にする)
         else
           render 'new'
         end
