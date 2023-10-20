d7d50a7bdec7108bd00af8cde0bf849a9fd245e2
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-10-20 07:44:48 +0000

    [add]admin_controller

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,33 @@
 class Admin::CustomersController < ApplicationController
+  before_action :get_customer_id, only: [:show, :edit, :update]
+  
+  def index
+    @customers = Customer.page(params[:page]).per(10)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @customer.update(customer_params)
+      flash[:notice] = "会員情報を更新しました。"
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_active)
+  end
+
+  def get_customer_id
+    @customer = Customer.find(params[:id])
+  end
+  
 end
