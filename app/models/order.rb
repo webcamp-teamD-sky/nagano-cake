7d1392a31c158c76ef61b9a30d6380d9b7390d44
myf2ar10 <myf2ar10@gmail.com>
2023-10-22 08:11:35 +0000

    [fix]enum
    注文ステータスと製作ステータスの定義を追加しました。

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
   belongs_to :customer
 
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum order_method: { waiting_payment: 0, confirmation: 1, in_production: 2, preparing_to_ship: 3, sent: 4 }
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,6 @@
 class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
+
+  enum production_method: { not_possible: 0, waiting: 1, in_production: 2, production_completed: 3 }
 end
