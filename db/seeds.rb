968bd206751c0e812be17ebe5adbfd0f51cf223c
myf2ar10 <myf2ar10@gmail.com>
2023-10-23 03:52:38 +0000

    [add]seed_customer
    customer用のseedを書いてみましたが上手く取り込めませんでした。後日改善します。

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,29 @@ Admin.create!(
    email: 'huwahuwacake@cake',
    password: 'cakehuwa'
 )
+
+#  10.times do |n|
+#     Customer.create!(
+#       last_name: "トステ#{n + 1}",
+#       first_name: "テスト#{n + 1}",
+#       last_name_kana: "とすて#{n + 1}",
+#       first_name_kana: "てすと#{n + 1}",
+#       phone_number: "01234567890",
+#       postcode: "1234567",
+#       email: "test#{n + 1}@test.com",
+#       encrypted_password: "123456",
+#       address: "TEST#{n + 1}"
+#     )
+#  end
+
+Customer.create!(
+   last_name: "トステ",
+   first_name: "テスト",
+   last_name_kana: "とすて",
+   first_name_kana: "てすと",
+   phone_number: "01234567890",
+   postcode: "1234567",
+   email: "test@test.com",
+   encrypted_password: "123456",
+   address: "TEST"
+)
