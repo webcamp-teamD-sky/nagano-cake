e511ceeec7c3fcf6c3a48433b22fe0b52e867159
myf2ar10 <myf2ar10@gmail.com>
2023-10-23 04:52:45 +0000

    [fix]new_check_controller
    viewの確認のためにcontrollerはいったんコメントアウトし、newページとcheckページを整えました。

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -46,48 +46,48 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    cart_items = current_customer.cart_items.all
-    @order = current_customer.orders.new(order_params)
-    @order.order_status = 0
-      if @order.save
-        current_customer.cart_items.each do |cart_item|
-          @ordered_item = OrderDetail.new
-          @ordered_item.order_id =  @order.id
-          @ordered_item.item_id = cart_item.item_id
-          @ordered_item.quantity = cart_item.amount
-          @ordered_item.price = (cart_item.item.price*1.1).floor
-          @ordered_item.save
-        end
-          redirect_to public_orders_complete_path
-            cart_items.destroy_all
-        else
-          render 'new'
-        end
-          @cart_items = current_customer.cart_items.all
-          @shipping_fee = 800
-          ary = []
-          @cart_items.each do |cart_item|
-            ary << (cart_item.item.price*1.1 *cart_item.amount).floor
-          end
-          @cart_items_price = ary.sum
-          @selected_pay_method = params[:order][:payment_method]
-          @total_price = @shipping_fee + @cart_items_price
+    # cart_items = current_customer.cart_items.all
+    # @order = current_customer.orders.new(order_params)
+    # @order.order_status = 0
+    #   if @order.save
+    #     current_customer.cart_items.each do |cart_item|
+    #       @ordered_item = OrderDetail.new
+    #       @ordered_item.order_id =  @order.id
+    #       @ordered_item.item_id = cart_item.item_id
+    #       @ordered_item.quantity = cart_item.amount
+    #       @ordered_item.price = (cart_item.item.price*1.1).floor
+    #       @ordered_item.save
+    #     end
+    #       redirect_to public_orders_complete_path
+    #         cart_items.destroy_all
+    #     else
+    #       render 'new'
+    #     end
+    #       @cart_items = current_customer.cart_items.all
+    #       @shipping_fee = 800
+    #       ary = []
+    #       @cart_items.each do |cart_item|
+    #         ary << (cart_item.item.price*1.1 *cart_item.amount).floor
+    #       end
+    #       @cart_items_price = ary.sum
+    #       @selected_pay_method = params[:order][:payment_method]
+    #       @total_price = @shipping_fee + @cart_items_price
   end
 
   def index
-    @orders = current_customer.orders.latest
-    @cart_items = current_customer.cart_items.all
+    # @orders = current_customer.orders.latest
+    # @cart_items = current_customer.cart_items.all
   end
 
   def show
-    @order = Order.find(params[:id])
-    @order_details = @order.order_details.all
-    @address = @order.postcode + @order.address + @order.address_name
+    # @order = Order.find(params[:id])
+    # @order_details = @order.order_details.all
+    # @address = @order.postcode + @order.address + @order.address_name
   end
 end
 
 private
 
 def order_params
-  params.require(:order).permit(:address_name, :address, :postcode, :payment_method, :billing_amount, :postage)
+  # params.require(:order).permit(:address_name, :address, :postcode, :payment_method, :billing_amount, :postage)
 end
