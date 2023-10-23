b9fd12ea22c948476e8180ddf375d829eadc5a2a
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-10-23 07:35:20 +0000

    [fix]Change in description

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,33 +1,34 @@
 class Admin::CustomersController < ApplicationController
-  before_action :get_customer_id, only: [:show, :edit, :update]
   
   def index
     @customers = Customer.page(params[:page]).per(10)
   end
 
   def show
+    @customer = Customer.find(params[:id])
+    @status   = Customer.select("is_deleted").find_by(id: params[:id]) 
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    
     if @customer.update(customer_params)
-      flash[:notice] = "会員情報を更新しました。"
-      redirect_to admin_customer_path(@customer)
+      redirect_to admin_customer_path(@customer), notice: "会員情報を更新しました。"
     else
-      render :edit
+      flash.now[:danger] = "エラーが発生しました"
+      @status   = Customer.select("is_deleted").find_by(id: params[:id]) 
+      render 'show'
     end
   end
-
+  
   private
-
+  
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_active)
-  end
-
-  def get_customer_id
-    @customer = Customer.find(params[:id])
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email, :is_deleted)
   end
   
 end
