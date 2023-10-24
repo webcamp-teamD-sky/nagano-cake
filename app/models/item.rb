951449c00a8a123bf41cbe4850be34096c981fe5
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-10-24 05:14:33 +0000

    [add]ActiveStorage_description

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,15 @@ class Item < ApplicationRecord
   has_many :customers, through: :cart_items
   has_many :order_details, dependent: :destroy
   belongs_to :genre
+  
+  has_one_atttached :image
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/noimage.jpg')
+      image.attach(io: File.open(file_path), filename: 'noimage.jpg', content_type: 'image/jpg')
+    end
+    iamge
+  end
+    
 end
