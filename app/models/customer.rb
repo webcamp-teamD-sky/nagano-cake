4114ff5848a3813174422230a69ad4f43259dec3
myf2ar10 <myf2ar10@gmail.com>
2023-10-24 08:32:13 +0000

    [fix]model
    customerとitemモデルにバリデーションを記述しました。

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -18,4 +18,15 @@ class Customer < ApplicationRecord
   # def full_name_kana
   #   self.last_name_kana + " " + self.first_name_kana
   # end
+  
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :phone_number, presence: true
+  validates :postcode, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :address, presence: true
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,15 @@ class Item < ApplicationRecord
   has_many :customers, through: :cart_items
   has_many :order_details, dependent: :destroy
   belongs_to :genre
-  
+
   has_one_attached :image
-  
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
+  validates :is_active, presence: true
+  validates :image, presence: true
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/noimage.jpg')
@@ -13,5 +19,5 @@ class Item < ApplicationRecord
     end
     iamge
   end
-    
+
 end
