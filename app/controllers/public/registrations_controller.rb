9133f5766be8310a1e099d70d68c642bfe8b069e
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-10-25 14:03:00 +0000

    [fix] sign_up

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -60,6 +60,15 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
 
+  # def after_sign_up_path_for(_resource)
+  #   customers_mypage_path
+  # end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postcode, :address,:phone_number, :password, :password_confirmation])  # 追加するパラメーターを記述
+  end
+
+  
   # 追加
   protected
 
