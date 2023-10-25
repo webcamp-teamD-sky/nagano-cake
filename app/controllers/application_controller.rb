ea4cda4a223cfc948a42af5872fc2fb9e9bdba7e
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-10-25 05:35:17 +0000

    [fix]admin_header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,16 @@ protected
   end
 
 
+private
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :customer
+        root_path
+    elsif resource_or_scope == :admin
+        new_admin_session_path
+    else
+        root_path
+    end
+  end
 
 
 
