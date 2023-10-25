15710c6176315c4b2adf5a7225de93ce994337fa
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-10-25 18:00:38 +0000

    [add] genre_search

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,6 +3,7 @@ class Public::HomesController < ApplicationController
   def top
     # @item = Item.find(params[:item_id])
     @items = Item.order(created_at: :desc).limit(4)
+    @genres = Genre.all
   end
 
   def about
